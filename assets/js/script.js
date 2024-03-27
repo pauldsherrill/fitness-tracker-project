807b729914e9e191b76ab5c4b5a8826fd000cf80
Chris Babb <ccbabb@gmail.com>
2024-03-26 20:07:34 -0500

    added javascript

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+let food = "4oz cheese";
+
 function getExercise() {
   fetch("https://exercisedb.p.rapidapi.com/exercises/bodyPart/back?limit=10", {
     method: "GET",
@@ -6,6 +8,21 @@ function getExercise() {
       "X-RapidAPI-Host": "exercisedb.p.rapidapi.com",
     },
   })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+    });
+}
+
+function getNutrition(food) {
+  fetch(
+    `https://api.edamam.com/api/nutrition-data?app_id=5a6e7bf2&app_key=52684844a0926b1e1b43dd3c939b92ad&nutrition-type=cooking&ingr=${food}`,
+    {
+      method: "GET",
+    }
+  )
     .then(function (response) {
       return response.json();
     })
