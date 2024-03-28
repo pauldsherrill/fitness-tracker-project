9507e27f3de4f2b211ff6c01875b112ae46e1c03
Omar Beltran <omarp.beltran9@gmail.com>
2024-03-28 17:06:34 -0500

    storage js

---
+++ assets/js/stg.js
@@ -0,0 +1,36 @@
+const mealInput = document.querySelector('#meal');
+const exerciseInput = document.querySelector('#exercise');
+
+renderLastRegistered();
+
+AddMealButton.addEventListener('click', function (event) {
+    event.preventDefault();
+  
+    const meal = mealInput.value;
+  
+    if (meal === '') {
+      displayMessage('error', 'meal cannot be blank');
+    } else {
+      displayMessage('success', 'Registered successfully');
+  
+      localStorage.setItem('exercise', email);
+
+      renderLastRegistered();
+    }
+  });
+
+  AddExerciseButton.addEventListener('click', function (event) {
+    event.preventDefault();
+  
+    const exercise = exerciseInput.value;
+  
+    if (exercise === '') {
+      displayMessage('error', 'exercise cannot be blank');
+    } else {
+      displayMessage('success', 'Registered successfully');
+  
+      localStorage.setItem('exercise', email);
+
+      renderLastRegistered();
+    }
+  });
